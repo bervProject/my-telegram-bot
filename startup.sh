724e4b28eeb2e2c3db73539c103a91384d76fbb0
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-11 15:50:54 +0700

    Setup log output and reset hook log

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn "tele_bot:create_app()" -b "0.0.0.0:${PORT}"
+gunicorn "tele_bot:create_app()" -b "0.0.0.0:${PORT}" --log-file --log-level debug
--- tele_bot/__init__.py
+++ tele_bot/__init__.py
@@ -15,6 +15,7 @@ token = os.environ.get("TELEGRAM_TOKEN", "")
 public_url = os.environ.get("PUBLIC_URL", "http://localhost:5000")
 bot = telebot.TeleBot(token)
 
+
 def test_pdf(message):
     if message is None:
         return False
@@ -22,13 +23,16 @@ def test_pdf(message):
         return False
     return message.document.mime_type == 'application/pdf'
 
-@bot.message_handler(commands=['start','help'])
+
+@bot.message_handler(commands=['start', 'help'])
 def handle_start_help(message):
     bot.reply_to(message, 'Please upload .pdf file to use this bot')
+
 
 @bot.message_handler(content_types=['photo', 'audio', 'sticker'])
 def not_supported_yet(message):
     bot.reply_to(message, 'Sorry, we not supported this yet')
+
 
 @bot.message_handler(func=test_pdf, content_types=['document'])
 def handle_message_doc(message):
@@ -36,7 +40,8 @@ def handle_message_doc(message):
     message_id = message.message_id
     user_id = message.from_user.id
     file_id = message.document.file_id
-    logger.info('get message {},{} from {} with file {}'.format(message_id, chat_id, user_id, file_id))
+    logger.info('get message {},{} from {} with file {}'.format(
+        message_id, chat_id, user_id, file_id))
     file_info = bot.get_file(file_id)
     doc_downloaded = bot.download_file(file_info.file_path)
     medias_plain = convert_pdf(doc_downloaded, user_id)
@@ -50,38 +55,47 @@ def handle_message_doc(message):
     for media in medias_plain:
         os.remove(media)
 
+
 def convert_pdf(pdf_byte, user_id):
     images = convert_from_bytes(pdf_byte, fmt="jpeg")
     print(len(images))
     current_uuid = uuid.uuid4()
     list_location = []
     for num, image in enumerate(images):
-        image_name = 'temp/{}/{}/output-{}.jpg'.format(user_id, current_uuid, num)
+        image_name = 'temp/{}/{}/output-{}.jpg'.format(
+            user_id, current_uuid, num)
         if not os.path.exists(os.path.dirname(image_name)):
             try:
                 os.makedirs(os.path.dirname(image_name))
-            except OSError as exc: # Guard against race condition
+            except OSError as exc:  # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
         image.save(image_name, format='JPEG')
         list_location.append(image_name)
     return list_location
 
-def create_app(test_config = None):
+
+def create_app(test_config=None):
     app = Flask(__name__)
+    app.logger.setLevel(logging.DEBUG)
     app.config.from_mapping(
         SECRET_KEY=os.environ.get('SECRET_KEY', 'dev-key')
     )
 
     @app.route('/' + token, methods=['POST'])
     def getMessage():
-        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        bot.process_new_updates(
+            [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
         return "!", 200
 
     @app.route("/reset-hook")
     def webhook():
+        app.logger.debug('Someone call reset hook')
+        app.logger.debug('Try to remove hook from bot')
         bot.remove_webhook()
+        app.logger.debug('Setup new webhook to: ' + public_url)
         bot.set_webhook(url=public_url + token)
+        app.logger.debug('Finished setup webhook to: ' + public_url)
         return render_template('success-reset.html')
 
     @app.route("/", methods=['GET'])
