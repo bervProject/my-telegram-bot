85a1093c263c3aae3080e1704ad58988ed840489
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-07-28 13:57:22 +0700

    fix: update resousce

--- infra/infra/infra_stack.py
+++ infra/infra/infra_stack.py
@@ -45,7 +45,7 @@ class TelegramBotInfraStack(Stack):
         task_execution_role.add_to_policy(iam.PolicyStatement(
             effect=iam.Effect.ALLOW,
             actions=["secretsmanager:GetSecretValue"],
-            resources=[f"arn:aws:secretsmanager:{self.region}:{self.account}:secret:dev/telegramBot*"]))
+            resources=[secret_arn.value_as_string]))
 
         # Task Role - application runtime permissions
         task_role = iam.Role(
